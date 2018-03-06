971c3965a88b71c935cb4bd1576e20fcd6949960
Arthur Brant <arthurcl.br@hotmail.com>
2018-03-06 18:33:51 -0300

    Routes para /contact e /about pages adicionado

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/contact', (req, res) => res.send('Contact Page'));
+
+app.get('/about', (req, res) => res.send('About Page'));
+
 app.listen(3000, () => console.log('App.js running at http://localhost:3000'));
