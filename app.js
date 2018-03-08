e27b769eb9f951f6cc251919182de4d2e39e904f
Arthur Brant <arthurcl.br@hotmail.com>
2018-03-08 19:57:32 -0300

    Instalado ejs como dependencia e engine view.

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/routes/index.html'));
 
 app.get('/contact', (req, res) => res.send('Contact Page'));
 
 app.get('/about', (req, res) => res.send('About Page'));
+
+app.get('/profile/:name', function (req, res) {
+    var data = {age: 25, job: 'student', hobbies: ['eating', 'fighting', 'fishing']};
+    res.render('profile', {person: req.params.name, data: data});
+});
 
 app.listen(3000, () => console.log('App.js running at http://localhost:3000'));
