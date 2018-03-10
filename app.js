99a1318f1a57bcd1250254493a8431caa61710a3
Arthur Brant <arthurcl.br@hotmail.com>
2018-03-10 17:35:10 -0300

    Retirado a utilizacao do EJS

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
+/* Evitar um pouco usar a view engine
 app.set('view engine', 'ejs');
+app.get('/profile/:name', function (req, res) {
+    var data = {age: 25, job: 'student', hobbies: ['eating', 'fighting', 'fishing']};
+    res.render('profile', {person: req.params.name, data: data});
+});
+*/
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/routes/index.html'));
 
@@ -9,9 +15,5 @@ app.get('/contact', (req, res) => res.send('Contact Page'));
 
 app.get('/about', (req, res) => res.send('About Page'));
 
-app.get('/profile/:name', function (req, res) {
-    var data = {age: 25, job: 'student', hobbies: ['eating', 'fighting', 'fishing']};
-    res.render('profile', {person: req.params.name, data: data});
-});
 
 app.listen(3000, () => console.log('App.js running at http://localhost:3000'));
