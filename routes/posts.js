201727852048c57961749ab841e1b49a0d41415f
Arthur Brant <arthurcl.br@hotmail.com>
2018-07-05 21:21:27 -0300

    Added a GET route that responds with contents of posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const posts_controller = require('../controllers/postsController');
-
 const router = express.Router();
 
-// GET the index page
 // router.get('/', posts_controller.index);
 
+// Handle GET of all posts
+router.get('/all', posts_controller.getAllPostsPromise);
+
 // GET a post
-router.get('/:slug', posts_controller.getPost, posts_controller.index);
+router.get('/:slug', posts_controller.getPostBySlug, posts_controller.index);
 
 module.exports = router;
