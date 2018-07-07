70c1c4f8224a77e1a8d82affeb8871fb0f89316c
Arthur Brant <arthurcl.br@hotmail.com>
2018-07-06 23:25:15 -0300

    app now use the ./route/posts

--- app.js
+++ app.js
@@ -2,21 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const posts = require('./posts.js');
+// const posts = require('./posts.js');
+const posts = require('./routes/posts');
+// const index = require('./routes/index'); NAO FUNCIONOU! SERVIDOR RESPONDEU QUE A PAGINA N EXISTIA!
 
-/**
- * middleware
-*/
+// Middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // const options = {maxAge: '10m'}; USE THIS OPTION IN PRODUCTION!
 app.use( express.static(path.join(__dirname, 'public')) );
 
-/**
- * routes
- */
+// Routes
 app.use('/post', posts);
-
+// app.use('/', index);
 app.get('/',(req, res) => {
     if (!req.xhr) {
         res.sendFile(path.join(__dirname, 'routes/index.html'));
