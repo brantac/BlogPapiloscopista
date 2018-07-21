961582d87431b0a3b7358d2392fb27add17d4421
Arthur Brant <arthurcl.br@hotmail.com>
2018-07-21 20:44:31 -0300

    indexRender() sends posts from buttercms

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,6 +4,20 @@ const fileOptions = {
     root: '.'
 }
 
+// Send the index.html
 exports.index = function (req, res, next) {
     res.sendFile(__dirname + '/routes/index.html');
+};
+
+// Send the index view (ESJ)
+exports.indexRender = (req, res, next) => {
+    /* posts_model.collectAllPosts()
+    .then( posts => {
+        console.log(typeof posts[0]);
+        res.render('index', {'posts': posts});
+    })
+    .catch( reason => console.log(reason) ); */
+    let posts = posts_model.getAll( (response) => {
+        res.render('index', {posts: response.data.data});
+    });
 };
