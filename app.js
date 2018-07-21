8b225fe7b5398c6be561e5afe55200f80e0a74d5
Arthur Brant <arthurcl.br@hotmail.com>
2018-07-21 20:47:34 -0300

    Using index route for the main page

--- app.js
+++ app.js
@@ -4,21 +4,22 @@ const path = require('path');
 const bodyParser = require('body-parser');
 // const posts = require('./posts.js');
 const posts = require('./routes/posts');
-// const index = require('./routes/index'); NAO FUNCIONOU! SERVIDOR RESPONDEU QUE A PAGINA N EXISTIA!
+const index = require('./routes/index');
+
+app.set('view engine', 'ejs');
 
 // Middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// const options = {maxAge: '10m'}; USE THIS OPTION IN PRODUCTION!
 app.use( express.static(path.join(__dirname, 'public')) );
 
 // Routes
 app.use('/post', posts);
-// app.use('/', index);
-app.get('/',(req, res) => {
+app.use('/', index);
+/* app.get('/',(req, res) => {
     if (!req.xhr) {
         res.sendFile(path.join(__dirname, 'routes/index.html'));
     }
-});
+}); */
 
 app.listen(3000, () => console.log('App.js running at http://localhost:3000'));
