80ade82c475f07dd1c7e33263f1a4c0cb8d1e2ff
Arthur Brant <arthurcl.br@hotmail.com>
2018-07-26 19:20:41 -0300

    add numbered page handler

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const index_controller = require('../controllers/indexController');
 const router = express.Router();
 
-// GET home page
-router.get('/', index_controller.indexRender);
+// Handle GET to index
+router.get('/', index_controller.index);
+
+// GET page
+router.get('/p/:page', index_controller.page);
 
 module.exports = router;
