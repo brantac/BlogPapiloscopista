c95859c65425a70195f5d1a56e52232ad34e4628
Arthur Brant <arthurcl.br@hotmail.com>
2018-08-23 12:01:40 -0300

    add email route handler

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,10 @@ const contact_controller = require('../controllers/contactController');
 const express = require('express');
 const router = express.Router();
 
-// GET author page
+// GET contact page
 router.get('/', contact_controller.contact);
+
+// POST contact form
+// router.get('/', contact_controller.sendEmail);
 
 module.exports = router;
