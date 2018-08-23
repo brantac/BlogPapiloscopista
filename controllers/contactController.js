e3f0185e2720ed804cd0f9b7784b0feb673ee19a
Arthur Brant <arthurcl.br@hotmail.com>
2018-08-23 12:03:00 -0300

    require nodemailer and write the simplest sender function

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,3 +1,31 @@
+const nodemailer = require('nodemailer');
+
+// render contact page
 exports.contact = (req, res) => {
     res.render('contact');
 }
+
+// send email
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: 'art.brant06@gmail.com',
+//         pass: 'aaa'
+//     }
+// });
+// const options = {
+//     from: '',
+//     to: '',
+//     subject: '',
+//     text: ''
+// };
+// transporter.sendMail(options, (error, info) => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Email sent: ' + info.response);
+//     }
+// });
+// exports.send = (req, res) => {
+
+// };
