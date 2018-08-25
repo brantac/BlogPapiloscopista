e10337fc69480540287897ea0e383bc58be6ae78
Arthur Brant <arthurcl.br@hotmail.com>
2018-08-24 22:49:29 -0300

    GET post using Prismic

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,7 @@ const posts_controller = require('../controllers/postsController');
 const router = express.Router();
 
 // GET a post
-router.get('/:slug', posts_controller.sendPost);
+// router.get('/:slug', posts_controller.sendPost);
+router.get('/:slug', posts_controller.sendPost2);
 
 module.exports = router;
