4bbff73e0f720ff6f7472ce78484461baa8b0d3b
Arthur Brant <arthurcl.br@hotmail.com>
2018-09-12 21:00:19 -0300

    remove functions that return posts stored locally

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,54 +1,5 @@
 const posts_model = require('../models/postsModel');
 
-// Send file options
-const sfOptions = {
-    root: "."
-};
-
-// Always send the index page
-exports.index = function (req, res, next) {
-    if ( !req.xhr ) {
-        res.sendFile('./routes/index.html', sfOptions);
-    } else {
-        res.end("<h3>Esta página não existe</h3>");
-    }
-};
-
-// Display json list of all posts
-exports.json_all_posts_list = function (req, res) {
-    res.send("NAO IMPLEMENTADO: json posts list");
-};
-
-// Display a specifc post
-exports.getPostBySlug = function (req, res, next) {
-    if (!req.xhr) {
-        next();
-    } else {
-        if (posts_model.postExist( req.params.slug, posts_model.blog_posts, "slug" )) {
-            res.sendFile( './routes/post/' + req.params.slug + '.html', sfOptions );
-        } else {
-            res.end("<h3>Este post não existe</h3>");
-        }
-    }
-};
-
-// Send the last post
-exports.getLastPost = function (req, res, next) {
-    let postSlug = posts_model.lastPostSlug(posts_model.blog_posts);
-    res.sendFile('./routes/post/' + postSlug + '.html', fileOptions);
-};
-
-// Send all posts stored in a directory
-exports.getAllPostsPromise = function (req, res, next) {
-    console.log("Posts controller: before get the promise.");
-    posts_model.collectAllPosts().then( (posts) => {
-        console.log("Posts controller: sending the promises.");
-        posts = posts.map( post => post.toString());
-        res.send(posts);
-    })
-    .catch( (err) => console.log(err));
-};
-
 // Prismic CMS
 exports.sendPost2 = (req, res) => {
     let slug = req.params.slug;
--- models/postsModel.js
+++ models/postsModel.js
@@ -1,103 +1,17 @@
-const fs = require('fs');
-// const butter = require('buttercms')('8b41c028f37bd170bd68bb4c9f4e3578950cfc0a');
+// Prismic module
 const Prismic = require('prismic-javascript');
 
-// Prismic API endpoint
+// Api endpoint
 const apiEndpoint = 'http://papiloscopiando.prismic.io/api/v2';
 
-/* Array list about all posts */
-exports.blog_posts = [
-    {"id": 1, "title": 'Primeiro Post', "slug": 'primeiro-post', "path": '/post/primeiro-post'},
-    {"id": 2, "title": 'Segundo Post', "slug": 'segundo-post', "path": '/post/segundo-post'},
-    {"id": 3, "title": 'Terceiro Post', "slug": 'terceiro-post', "path": '/post/terceiro-post'}
-];
-
-/**
- * Try to find the post requested.
- * 'reqSlug' is the name of the post requested.
- * 'list' is the list of posts info.
- * 'key' is the object key used to compare with the slug.
- * NOTE: REWRITE THE FUNCTION USING .MAP(), WHICH IS SIMPLER AND ELEGANT
- */
-exports.postExist = (reqSlug = "", list = [], key = "") => {
-    var pLen = list.length;
-    var postFound = false;
-    var ind = 0;
-    while (!postFound && ind < pLen) {
-        if (reqSlug == list[ind][key]) {
-            postFound = true;
-        }
-        ind++;
-    }
-    return postFound;
-};
-
-// Returns the slug for the last post
-exports.lastPostSlug = (list = []) => {
-    let l = list.length - 1;
-    return list[l][slug];
-};
-
-/**
- * Read filenames inside a directory and then
- * read all files content inside that directory
- * using Promises.
- */
-// Read the filenames asynchronously
-let readdirAsync =  (dir) => {
-    return new Promise( (resolve, reject) => {
-        fs.readdir(dir, (err, filenames) => {
-            if (err) reject(err);
-            resolve(filenames); 
-        });
-    });
-};
-
-// Read one file asynchronously
-let readFileAsync = (filename, enc) => {
-    return new Promise( (resolve, reject) => {
-        fs.readFile('./routes/post/' + filename, enc, (err, data) => {
-            if (err) reject(err);
-            resolve(data);
-        });
-    });
-};
-
-// Utility function that returns a Promise
-let getFile = (filename) => {
-    return readFileAsync(filename, 'utf8');
-};
-
-// Returns all posts as a Promise
-exports.collectAllPosts = () => {
-    return readdirAsync('./routes/post/').then( (filenames) => {
-        console.log('Filenames: ' + filenames);
-        return Promise.all( filenames.map(getFile) )
-    })
-    .catch( (reason) => reason);
-};
-
-// Butter CMS API calls
-// fetch all posts in one page
-// exports.list = (fn, page = 1) => {
-//     return butter.post.list({page: page, page_size: 10})
-//     .then( fn )
-//     .catch( reason => console.log(reason) );
-// };
-// // fetch a specific post
-// exports.getpost = (slug, fn1, fn2) => {
-//     return butter.post.retrieve(slug)
-//     .then( fn1 )
-//     .catch( fn2 );
-// };
-
-// Prismic CMS API calls
-function initApi (req) {
+// Prismic initiator function
+let initApi = (req) => {
     return Prismic.getApi(apiEndpoint, {
         req: req
     });
 }
 
+// Return one post
 exports.queryPost = () => {
     return function (req, slug, promiseHandlers) {
         return initApi(req).then( (api) => {
@@ -108,6 +22,7 @@ exports.queryPost = () => {
     };
 };
 
+// Return a list of posts
 exports.queryList =  () => {
     return function (req, promiseHandlers) {
         return initApi(req).then( (api) => {
