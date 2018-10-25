51f088773e0299c64804ecab8639f7efead02e79
Arthur Brant <arthurcl.br@hotmail.com>
2018-10-25 11:55:22 -0300

    add videos to the videos page

--- controllers/videosController.js
+++ controllers/videosController.js
@@ -1,12 +1,31 @@
+const Prismic = require('prismic-javascript');
+const videosModel = require('../models/videosModel');
+const apiEndpoint = 'http://papiloscopiando.prismic.io/api/v2';
+
 exports.videos = (req, res) => {
-    res.render('videos', {
-        page: 'videos',
-        slug: 'videos',
-        seo: {
-            title: 'Videos | ',
-            description: 'Fique por dentro das novidades sobre a papiloscopia' +
-            ' no Brasil e no mundo. Compartilhamos conhecimento, compartilhamos cidadania.',
-            image: '/img/icones/blog_icon_15px.png'
-        }
+    Prismic.getApi(apiEndpoint, {req}).then((api) => {
+        return videosModel.getVideos(api, req.params.page);
+    })
+    .then((documents) => {
+
+        /** 
+         * Object
+         */
+        let renderData = {
+            page: 'videos',
+            slug: 'videos',
+            seo: {
+                title: 'Videos | ',
+                description: 'Fique por dentro das novidades sobre a papiloscopia' +
+                ' no Brasil e no mundo. Compartilhamos conhecimento, compartilhamos cidadania.',
+                image: '/img/icones/blog_icon_15px.png'
+            },
+            prismicDocuments: documents
+        };
+
+        res.status(200).render('videos', renderData);
+    })
+    .catch((err) => {
+        res.status(500).send(`Erro: ${err.message}`);
     });
-}
+};
--- public/libs/utils/prismic-config.js
+++ public/libs/utils/prismic-config.js
@@ -1,4 +1,5 @@
 exports.linkResolver = function (doc, ctx) {
     if (doc.type === 'blog_post') return '/post/' + doc.uid;
+    if (doc.type === 'blog_video') return '/videos/' + doc.uid;
     return '/';
 };
