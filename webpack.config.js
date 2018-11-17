a84f08f0b565d6970ae9181ed8af098aa405b7cd
Arthur Brant <arthurcl.br@hotmail.com>
2018-11-17 00:21:58 -0200

    add image and font loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,95 @@
+const webpack = require('webpack');
 const path = require('path');
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WebpackMd5Hash = require('webpack-md5-hash');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+let isDevMode =  (() => {
+    if (process.env.NODE_ENV === 'development') return true;
+    return false;
+})();
 
 const config = {
-    entry: {main: './index.js'},
+    entry: {
+        main: [
+            './index.js',
+            './public/css/output.css'
+        ]
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[chunkhash].js',
         path: path.resolve(__dirname, 'public/dist')
     },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: false
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: (() => {
+                if (isDevMode) return "[name].css";
+                return "[name].[contenthash].css";
+            })(),
+            chunkFilename: (() => {
+                if (isDevMode) return "[id].css";
+                return "[id].[contenthash].css";
+            })()
+        }),
+        new WebpackMd5Hash()
+    ],
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.(sa|c)ss$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            includePaths: [
+                                path.resolve(__dirname, './node_modules')
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            bypassOnDebug: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|otf)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "fonts/"
+                        }
+                    }
                 ]
             }
         ]
