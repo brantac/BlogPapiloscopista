2034c348b31a8501397ccc1866b30c849de7fa42
Arthur Brant <arthurcl.br@hotmail.com>
2019-02-12 16:28:25 -0200

    add try/catch to catch errors in the analytics api

--- models/googleAnalyticsModel.js
+++ models/googleAnalyticsModel.js
@@ -2,42 +2,47 @@ const {google} = require("googleapis");
 
 async function main () {
     
-    // This method looks for the GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIALS
-    // environment variables.
-    const auth = await google.auth.getClient({
-        // Scope of the analytics reporting,
-        // with only reading access.
-        scopes: 'https://www.googleapis.com/auth/analytics.readonly',
-    });
+    try {
+    
+        // This method looks for the GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIALS
+        // environment variables.
+        const auth = await google.auth.getClient({
+            // Scope of the analytics reporting,
+            // with only reading access.
+            scopes: 'https://www.googleapis.com/auth/analytics.readonly',
+        });
 
-    // Create the analytics reporting object
-    const analyticsreporting = await google.analyticsreporting({
-        version: 'v4',
-        auth: auth,
-    });
+        // Create the analytics reporting object
+        const analyticsreporting = await google.analyticsreporting({
+            version: 'v4',
+            auth: auth,
+        });
 
-    // Fetch the analytics reporting
-    const res = await analyticsreporting.reports.batchGet({
-        requestBody: {
-            reportRequests: [
-                {
-                    viewId: '183362896',
-                    dateRanges: [
-                        {
-                            startDate: '2018-09-01',
-                            endDate: '2018-11-30',
-                        },
-                    ],
-                    metrics: [
-                        {
-                            expression: 'ga:pageviews',
-                        },
-                    ],
-                },
-            ],
-        },
-    });
-    return res.data;
+        // Fetch the analytics reporting
+        const res = await analyticsreporting.reports.batchGet({
+            requestBody: {
+                reportRequests: [
+                    {
+                        viewId: '183362896',
+                        dateRanges: [
+                            {
+                                startDate: '2018-09-01',
+                                endDate: '2018-11-30',
+                            },
+                        ],
+                        metrics: [
+                            {
+                                expression: 'ga:pageviews',
+                            },
+                        ],
+                    },
+                ],
+            },
+        });
+        return res.data;
+    } catch (error) {
+        return error;       
+    }
 }
 
 module.exports = {
