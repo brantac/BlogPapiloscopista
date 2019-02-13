816442d06065ece1338616f1f1db49f349e23d97
Arthur Brant <arthurcl.br@hotmail.com>
2019-02-13 12:33:47 -0200

    remove reference to service account JSON credentials

--- models/googleAnalyticsModel.js
+++ models/googleAnalyticsModel.js
@@ -9,7 +9,6 @@ async function main () {
         const auth = await google.auth.getClient({
             // Scope of the analytics reporting,
             // with only reading access.
-            keyFile: '../papiloscopiando_gaservice_keyfile.json',
             scopes: 'https://www.googleapis.com/auth/analytics.readonly',
         });
 
